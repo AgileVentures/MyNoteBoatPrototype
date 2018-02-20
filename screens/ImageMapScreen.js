8aee24e594ac3748f32199f19f7d99c0f90a768f
Sam Joseph <tansaku@gmail.com>
2018-02-20 17:04:39 +0000

    experimenting with touch functionality

--- screens/ImageMapScreen.js
+++ screens/ImageMapScreen.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Dimensions, StyleSheet, View } from 'react-native';
 import { VictoryChart, VictoryStack, VictoryArea } from 'victory-native';
-import Svg, { Text } from 'react-native-svg';
+import Svg, {
+    Circle,
+    Path,
+    Rect,
+    G,
+    Text,
+    ClipPath,
+    Defs
+} from 'react-native-svg';
 import { Font } from 'expo';
 
 export default class ImageMapScreen extends React.Component {
@@ -9,49 +17,58 @@ export default class ImageMapScreen extends React.Component {
     title: '<Svg />',
   };
 
+  constructor () {
+      super(...arguments);
+      this.state = {
+          hover: false
+      };
+  }
+
+  toggle = () => {
+      this.setState({hover: !this.state.hover});
+  };
+
+
   render() {
     return (
       <View style={styles.container}>
-        <VictoryChart>
-          <VictoryStack>
-            <VictoryArea
-              data={[
-                { x: 'a', y: 2 },
-                { x: 'b', y: 3 },
-                { x: 'c', y: 5 },
-                { x: 'd', y: 4 },
-                { x: 'e', y: 7 },
-              ]}
-            />
-            <VictoryArea
-              data={[
-                { x: 'a', y: 1 },
-                { x: 'b', y: 4 },
-                { x: 'c', y: 5 },
-                { x: 'd', y: 7 },
-                { x: 'e', y: 5 },
-              ]}
-            />
-            <VictoryArea
-              data={[
-                { x: 'a', y: 3 },
-                { x: 'b', y: 2 },
-                { x: 'c', y: 6 },
-                { x: 'd', y: 2 },
-                { x: 'e', y: 6 },
-              ]}
-            />
-            <VictoryArea
-              data={[
-                { x: 'a', y: 2 },
-                { x: 'b', y: 3 },
-                { x: 'c', y: 3 },
-                { x: 'd', y: 4 },
-                { x: 'e', y: 7 },
-              ]}
-            />
-          </VictoryStack>
-        </VictoryChart>
+        <Svg
+            height="100"
+            width="100"
+        >
+          <Circle
+            cx="50%"
+            cy="50%"
+            r="38%"
+            fill="red"
+            onPress={() => alert('Press on Circle')}
+          />
+        </Svg>
+
+        <Svg height="120" width="120">
+            <Defs>
+                <ClipPath id="clip">
+                    <Circle r="80" cx="50%" cy="50%"/>
+                </ClipPath>
+            </Defs>
+            <G>
+                <G>
+                    <Path
+                        d="M50,5L20,99L95,39L5,39L80,99z"
+                        clipPath="url(#clip)"
+                        stroke={this.state.hover ? 'rgba(10, 10, 10, 0.5)' : 'black'}
+                        fill={this.state.hover ? 'pink' : 'red'}
+                        strokeWidth="6"
+                        delayPressIn={0}
+                        onPressIn={this.toggle}
+                        onPressOut={this.toggle}
+                        x="0"
+                        y="0"
+                        scale="1.2"
+                    />
+                </G>
+            </G>
+        </Svg>
 
         <Svg width={Dimensions.get('window').width} height={50}>
           <Text
@@ -60,8 +77,9 @@ export default class ImageMapScreen extends React.Component {
             fontSize={15}
             fontFamily={Font.processFontFamily('space-mono')}
             x={25}
-            y={15}>
-            drawn with victory-native
+            y={15}
+            style="display:hidden;">
+            drawn with react-native-svg
           </Text>
         </Svg>
       </View>
