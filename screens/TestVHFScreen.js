78144cad44c2dc7c0bf414feed674a1996ee4b89
Sam Joseph <tansaku@gmail.com>
2018-02-21 16:06:37 +0000

    persists data to local store

--- screens/TestVHFScreen.js
+++ screens/TestVHFScreen.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableHighlight } from 'react-native';
+import { AsyncStorage, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 var t = require('tcomb-form-native');
 
 var Form = t.form.Form;
 
 var TestVHF = t.struct({
-  installed: t.Date,              // a required date
-  checked: t.Date,               // a required date
-  cost: t.maybe(t.String),     // an optional string
-  rememberMe: t.Boolean        // a boolean
+  installed: t.String,              // a required string
+  checked: t.String,               // a required string
+  cost: t.maybe(t.String)     // an optional string
 });
 
-var options = {}; // optional rendering options (see documentation)
-
+const options = {
+  // fields: {
+  //   installed: {
+  //     config: {
+  //       format: date => format(date, "DD-MM-YYYY")
+  //     }
+  //   }
+  // }
+};
 
 export default class TestVHFScreen extends React.Component {
   static navigationOptions = {
@@ -24,22 +30,75 @@ export default class TestVHFScreen extends React.Component {
     super(props);
 
     this.onPress = this.onPress.bind(this);
+    // var value = await this.loadStoredData()
+    // console.log('this is what we are setting');
+    // console.log(value);
+    // this.state = {
+    //   value: value //{ checked: "recently",
+    //            //installed: "long ago" } 
+    // };
   }
 
-  onPress() {
+  state = {
+    isLoading: true
+  };
+
+  componentDidMount() {
+    AsyncStorage.getItem('@MyNoteBoatStore:TestVHF').then((value) => {
+      if (value === null){ value = "{}" }
+      this.setState({
+        isLoading: false,
+        value: JSON.parse(value)
+      });
+    });
+  }
+
+  async loadStoredData() {
+    var value = "{}"
+    try {
+      value = await AsyncStorage.getItem('@MyNoteBoatStore:TestVHF');
+      if (value !== null){
+        console.log("loaded some data");
+        console.log(value);
+      }
+    } catch (error) {
+      value = "{}"
+      console.log("could not retrieve data")
+      console.log(error)
+    }
+    return JSON.parse(value);
+  }
+
+  async onPress() {
     // call getValue() to get the values of the form
     var value = this.refs.form.getValue();
     if (value) { // if validation fails, value will be null
+      console.log("received form input");
       console.log(value); // value here is an instance of Person
+      try {
+        await AsyncStorage.setItem('@MyNoteBoatStore:TestVHF', JSON.stringify(value));
+      } catch (error) {
+        console.log("could not save data")
+        console.log(error)
+      }
     }
   };
 
+  // onChange(value) {
+  //   this.setState({value});
+  // }
+
   render() {
+    if (this.state.isLoading) {
+      return <View><Text>Loading...</Text></View>;
+    }
     return (
       <View style={styles.container}>
          <Form
           ref="form"
           type={TestVHF}
+          value={this.state.value}
+          // onChange={this.onChange}
           options={options}
         />
         <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
